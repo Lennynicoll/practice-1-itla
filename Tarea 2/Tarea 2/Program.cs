e5457ce3683741b9c5a613b9d162a68bdee1bfdd
Lenny Nuñez <154910365+Lennynicoll@users.noreply.github.com>
2026-02-10 17:13:31 -0400

    Entregando tarea 2

---
+++ Tarea 2/Tarea 2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Lenny Nuñez 2025-1878
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Escribe un número: ");
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int number))
+        {
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("El número es par.");
+            }
+            else
+            {
+                Console.WriteLine("El número es impar.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Entrada inválida. Debes escribir un número entero.");
+        }
+    }
+}
